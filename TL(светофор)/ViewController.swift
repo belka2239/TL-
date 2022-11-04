65a304d29ecf29397a10067e5aa9f8f06c80517b
belka2239 <belka2239@gmail.com>
2022-11-04 12:01:58 +0300

    Initial Commit

---
+++ TL(светофор)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TL(светофор)
+//
+//  Created by VG on 04.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
